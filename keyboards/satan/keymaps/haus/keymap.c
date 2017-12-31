8b8f56a63c290d21121c83abb0b02ebf25f6308a
Marcus Grant <marcus.grant@patternbuffer.io>
2017-12-31 02:38:47 -0500

    first version of my haus configuration for satan gh60 boards

--- keyboards/satan/keymaps/haus/keymap.c
+++ keyboards/satan/keymaps/haus/keymap.c
@@ -1,3 +1,47 @@
+/* Haus Mapping from Mark Haus of Pattern Buffer & HausLabs
+ * Based off pok3r mapping with a few modifications
+ * Modifications from Pok3r:
+ * - Bottom Row modifiers:
+ *   - Right from Space Bar:
+ *      1. Fn
+ *      2. Right Alt
+ *      3. Right GUI // TODO: Will be the macro layer in future
+ *      4. Right Ctrl
+ * - Home row modifications:
+ *   - Caps is now Esc
+ * - Top row modifications:
+ *   - Grave(Backtick)/Tilde are now in place of Esc
+ * - Function Layer Modifications:
+ *   - Row below home row modifications:
+ *      - Media section instead of page up/down and other directionals
+ *      - From n key to right:
+ *          1. Volume down
+ *          2. Volume up
+ *          3. Toggle Mute
+ *          4. Toggle Play/Pause
+ *   - Home row:
+ *     1. Left
+ *     2. Down
+ *     3. Right
+ *     4. Page Down
+ *     5. Nothing Else in home row function layer after page down
+ *   - Above Home Row:
+ *     1. Home
+ *     2. Up
+ *     3. End
+ *     4. Page Up
+ *     5. Nothing
+ *     6. Calculator (same as Pok3r)
+ *     7. Nothing
+ *     8. Nothing
+ *     9. Nothing
+ *     10. Nothing (instead of KC_SCLK)
+ *     11. Print Screen
+ *   - Top Row Function Layer:
+ *      1. Caps lock is Fn+Esc
+ *          - Only modification from Pok3r in this row
+ *
+ */
 #include "satan.h"
 
 // Used for SHIFT_ESC
@@ -14,42 +58,44 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,-----------------------------------------------------------.
-   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
+   * |~ ` | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
    * |-----------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
    * |-----------------------------------------------------------|
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |Esc    |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
    * |-----------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
    * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |FN  |Ctrl |
+   * |Ctrl|Gui |Alt |      Space            |FN |Alt |Gui  |Ctrl |
    * `-----------------------------------------------------------'
    */
+// TODO: Right Gui should eventually become an additional layer, probably for macros
 [_BL] = KEYMAP_ANSI(
-  F(0),    KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+  KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-  KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  \
+  KC_ESC,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  \
   KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,                   KC_RSFT, \
-  KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                             KC_RALT, MO(_RL), MO(_FL), KC_RCTL),
+  KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                             MO(_FL), KC_RALT, KC_LGUI, KC_RCTL),
 
+// TODO: Implement TMUX macro for prefix
 /* Keymap _FL: Function Layer
    * ,-----------------------------------------------------------.
-   * | ~ | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|  DEL  |
+   * |CAP| F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|  DEL  |
    * |-----------------------------------------------------------|
-   * |     |   |Up|  |  |  |Cal|   |Ins|   |PrSc|Sclk|Paus|      |
+   * |     |Hme|Up|Ed|Pu|  |Cal|   |Ins|   |    |Pscr|    |      |
    * |-----------------------------------------------------------|
-   * |      |Left|Down|Rig|  |  |  |  |   |   |Home|PgUp|        |
+   * |      |Left|Down|Rig|Pd|  |  |  |   |   |    |    |        |
    * |-----------------------------------------------------------|
-   * |        |   |App|  |  |   |Vdn|Vup|Mute|End|PgDn|          |
+   * |        |   |App|  |  |   |Vdn|Vup|Mute|Paus|   |          |
    * |-----------------------------------------------------------|
    * |    |    |    |                        |    |    |    |    |
    * `-----------------------------------------------------------'
    */
 [_FL] = KEYMAP_ANSI(
-  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-  KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_CALC, KC_TRNS,  KC_INS,  KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, \
-  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP,          KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_APP,  KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU,  KC_MUTE, KC_END,  KC_PGDN,                   KC_TRNS, \
+  KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
+  KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_CALC, KC_TRNS,  KC_INS,  KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_APP,  KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU,  KC_MUTE, KC_PAUS, KC_TRNS,                   KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
   /* Keymap _RL: Function Layer
@@ -65,6 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |                        |    |    |    |    |
    * `-----------------------------------------------------------'
    */
+// TODO: Since I'm uninterested in LED mapping on most main entry keyboards, implement a macro layer here
 [_RL] = KEYMAP_ANSI(
   #ifdef RGBLIGHT_ENABLE
   KC_GRV,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   \
@@ -80,6 +127,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
   #endif
 };
+
+
+// TODO: Which of these are necessary after modifying Pok3r layout?
 
 enum function_id {
     SHIFT_ESC,
